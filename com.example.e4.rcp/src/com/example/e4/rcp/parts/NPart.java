d26bc5b203b6afc1470e8561f52dd6fa620ae7ab
Christian Femers <femers@in.tum.de>
2019-04-29 12:57:37 +0200

    Basic Test

---
+++ com.example.e4.rcp/src/com/example/e4/rcp/parts/NPart.java
@@ -0,0 +1,22 @@
+ 
+package com.example.e4.rcp.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class NPart {
+	@Inject
+	public NPart() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
