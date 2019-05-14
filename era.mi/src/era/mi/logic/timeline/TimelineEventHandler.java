c6457e199eb10be0ff207bac9a41db7f4a159964
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-14 15:15:25 +0200

    Removed obsolete era.mi in master branch

--- era.mi/src/era/mi/logic/timeline/TimelineEventHandler.java
+++
@@ -1,6 +0,0 @@
-package era.mi.logic.timeline;
-
-public interface TimelineEventHandler
-{
-	public void handle(TimelineEvent e);
-}
--- era.mi/src/era/mi/logic/wires/WireArrayObserver.java
+++
@@ -1,6 +0,0 @@
-package era.mi.logic.wires;
-
-public interface WireArrayObserver
-{
-	public void update(WireArray initiator);
-}
