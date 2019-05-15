9a2d3f18de74d15dc9507d124d5d3b62a2a4d53e
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-15 11:47:31 +0200

    Added comments in BasicGUIComponent

--- LogicUI/src/era/mi/components/gui/BasicGUIComponent.java
+++ LogicUI/src/era/mi/components/gui/BasicGUIComponent.java
@@ -6,9 +6,23 @@ import net.haspamelodica.swt.helper.swtobjectwrappers.Point;
 
 public interface BasicGUIComponent
 {
+	/**
+	 * Render this component to the given gc, at coordinates (0, 0).
+	 */
 	public void render(GeneralGC gc);
 
+	//TODO this code will be replaced by code in BasicComponent.
+	/**
+	 * Returns how many wire arrays are connected to this component.
+	 * (Connections are static - they can't be removed and no new ones can be added)
+	 */
 	public int getConnectedWireArraysCount();
+	/**
+	 * Returns the n-th wire array connected to this component.
+	 */
 	public WireArray getConnectedWireArray(int connectionIndex);
+	/**
+	 * Returns relative coordinates where the n-th wire array is connected to this component.
+	 */
 	public Point getWireArrayConnectionPoint(int connectionIndex);
 }
