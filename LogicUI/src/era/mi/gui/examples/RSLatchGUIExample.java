7d072d3bfb57ea4b55bee70639cfb673728c1f43
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-16 11:07:18 +0200

    Made RS latch example more similar to GUITest

--- LogicUI/src/era/mi/gui/examples/RSLatchGUIExample.java
+++ LogicUI/src/era/mi/gui/examples/RSLatchGUIExample.java
@@ -39,8 +39,10 @@ public class RSLatchGUIExample
 		GUINotGate not1 = ui.addComponent(new GUINotGate(NOT_DELAY, t1, q), 200, 107.5);
 		GUINotGate not2 = ui.addComponent(new GUINotGate(NOT_DELAY, t2, nq), 200, 197.5);
 
-		WireConnectionPoint p1 = ui.addComponent(new WireConnectionPoint(q, 2), 250, 112.5);
-		WireConnectionPoint p2 = ui.addComponent(new WireConnectionPoint(nq, 2), 250, 202.5);
+		WireConnectionPoint p1 = ui.addComponent(new WireConnectionPoint(q, 3), 250, 112.5);
+		WireConnectionPoint p2 = ui.addComponent(new WireConnectionPoint(nq, 3), 250, 202.5);
+		WireConnectionPoint o1 = ui.addComponent(new WireConnectionPoint(q, 1), 270, 112.5);
+		WireConnectionPoint o2 = ui.addComponent(new WireConnectionPoint(nq, 1), 270, 202.5);
 
 		ui.addWire(rIn, 0, or1, 0);
 		ui.addWire(sIn, 0, or2, 1);
@@ -50,5 +52,7 @@ public class RSLatchGUIExample
 		ui.addWire(not2, 1, p2, 0);
 		ui.addWire(p1, 1, or2, 0, new Point(250, 130), new Point(140, 185), new Point(140, 197.5));
 		ui.addWire(p2, 1, or1, 1, new Point(250, 185), new Point(140, 130), new Point(140, 117.5));
+		ui.addWire(p1, 2, o1, 0);
+		ui.addWire(p2, 2, o2, 0);
 	}
 }
