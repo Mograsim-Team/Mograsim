953ad2ae43cdaf494bfecbf4fe3770c9789f4613
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-16 11:16:08 +0200

    GUIManualSwitch now shows the actual value of the output WireArray

--- LogicUI/src/era/mi/gui/components/GUIManualSwitch.java
+++ LogicUI/src/era/mi/gui/components/GUIManualSwitch.java
@@ -2,8 +2,11 @@ package era.mi.gui.components;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import era.mi.logic.Bit;
 import era.mi.logic.components.ManualSwitch;
 import era.mi.logic.wires.WireArray;
 import net.haspamelodica.swt.helper.gcs.GeneralGC;
@@ -13,12 +16,27 @@ import net.haspamelodica.swt.helper.swtobjectwrappers.Rectangle;
 
 public class GUIManualSwitch extends ManualSwitch implements BasicGUIComponent
 {
+	private static final Map<Bit, String> bitNames;
+	static
+	{
+		Map<Bit, String> bitNamesModifiable = new HashMap<>();
+		bitNamesModifiable.put(Bit.ONE, "1");
+		bitNamesModifiable.put(Bit.ZERO, "0");
+		bitNamesModifiable.put(Bit.Z, "Z");
+		bitNamesModifiable.put(Bit.U, "U");
+		bitNamesModifiable.put(Bit.X, "X");
+		bitNames = Collections.unmodifiableMap(bitNamesModifiable);
+	}
+
+	private final WireArray			wa;
 	private final List<WireArray>	connectedWireArrays;
 	private final List<Point>		wireArrayConnectionPoints;
 
 	public GUIManualSwitch(WireArray output)
 	{
 		super(output);
+
+		this.wa = output;
 
 		List<WireArray> connectedWireArraysModifiable = new ArrayList<>();
 		List<Point> wireArrayConnectionPointsModifiable = new ArrayList<>();
@@ -39,7 +57,7 @@ public class GUIManualSwitch extends ManualSwitch implements BasicGUIComponent
 	public void render(GeneralGC gc)
 	{
 		gc.drawRectangle(0, 0, 20, 15);
-		String label = isOn() ? "ON" : "OFF";
+		String label = bitNames.get(wa.getValue());
 		Font oldFont = gc.getFont();
 		Font labelFont = new Font(oldFont.getName(), 6, oldFont.getStyle());
 		gc.setFont(labelFont);
