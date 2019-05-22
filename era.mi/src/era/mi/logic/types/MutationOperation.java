00f734d3e0e93447eb5098767ddb1fb0b9edebeb
Christian Femers <femers@in.tum.de>
2019-05-22 19:29:58 +0200

    Some minor style and declaration improvements

--- era.mi/src/era/mi/logic/types/MutationOperation.java
+++ era.mi/src/era/mi/logic/types/MutationOperation.java
@@ -7,5 +7,5 @@ import era.mi.logic.types.BitVector.BitVectorMutator;
 @FunctionalInterface
 public interface MutationOperation extends BiFunction<BitVectorMutator, BitVector, BitVectorMutator>
 {
-
+	// no changes necessary, only for convenience and readability
 }
