49673cc174c09d152032f8136d9b66df3828b736
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-23 13:39:29 +0200

    LogicUI now works as a ERCP part!

---
+++ SampleERCP/src/sampleercp/parts/LogicUIPart.java
@@ -0,0 +1,54 @@
+package sampleercp.parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import era.mi.gui.LogicUICanvas;
+import era.mi.gui.examples.RSLatchGUIExample;
+import era.mi.logic.Simulation;
+import net.haspamelodica.swt.helper.zoomablecanvas.helper.ZoomableCanvasUserInput;
+
+public class LogicUIPart
+{
+	@Inject
+	private MPart part;
+
+	@PostConstruct
+	public void create(Composite parent)
+	{
+		LogicUICanvas ui = new LogicUICanvas(parent, SWT.NONE);
+		RSLatchGUIExample.addComponentsAndWires(ui);
+		ui.addTransformListener((x, y, z) -> part.setDirty(z < 1));
+		ZoomableCanvasUserInput userInput = new ZoomableCanvasUserInput(ui);
+		userInput.buttonDrag = 3;
+		userInput.buttonZoom = 2;
+		userInput.enableUserInput();
+		Thread simulationThread = new Thread(() ->
+		{
+			// TODO find a better condition
+			while (!ui.isDisposed())
+			{
+				// always execute to keep timeline from "hanging behind" for too long
+				Simulation.TIMELINE.executeUpTo(System.currentTimeMillis(), System.currentTimeMillis() + 10);
+				long sleepTime;
+				if (Simulation.TIMELINE.hasNext())
+					sleepTime = Simulation.TIMELINE.nextEventTime() - System.currentTimeMillis();
+				else
+					sleepTime = 10;
+				try
+				{
+					if (sleepTime > 0)
+						Thread.sleep(sleepTime);
+				}
+				catch (InterruptedException e)
+				{
+				} // it is normal execution flow to be interrupted
+			}
+		});
+		simulationThread.start();
+	}
+}
