847c715730cefc84768ab2012e8d3e6265717c98
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-05-29 16:57:19 +0200

    Added adapters for OrGate and NotGate

--- LogicUI/src/era/mi/gui/modeladapter/componentadapters/SimpleGateAdapter.java
+++ LogicUI/src/era/mi/gui/modeladapter/componentadapters/SimpleGateAdapter.java
@@ -3,20 +3,26 @@ package era.mi.gui.modeladapter.componentadapters;
 import java.util.List;
 import java.util.Map;
 
-import era.mi.gui.model.components.GUIAndGate;
+import era.mi.gui.model.components.SimpleRectangularGUIGate;
 import era.mi.gui.model.wires.Pin;
 import era.mi.gui.modeladapter.LogicModelParameters;
 import era.mi.logic.components.Component;
-import era.mi.logic.components.gates.AndGate;
 import era.mi.logic.timeline.Timeline;
 import era.mi.logic.wires.Wire;
 import era.mi.logic.wires.Wire.ReadEnd;
 import era.mi.logic.wires.Wire.ReadWriteEnd;
 
-public class AndGateAdapter implements ComponentAdapter<GUIAndGate>
+public class SimpleGateAdapter implements ComponentAdapter<SimpleRectangularGUIGate>
 {
+	private final ComponentConstructor constructor;
+
+	public SimpleGateAdapter(ComponentConstructor constructor)
+	{
+		this.constructor = constructor;
+	}
+
 	@Override
-	public Component createAndLinkComponent(Timeline timeline, LogicModelParameters params, GUIAndGate guiComponent,
+	public Component createAndLinkComponent(Timeline timeline, LogicModelParameters params, SimpleRectangularGUIGate guiComponent,
 			Map<Pin, Wire> logicWiresPerPin)
 	{
 		ReadWriteEnd out = logicWiresPerPin.get(guiComponent.getOutputPin()).createReadWriteEnd();
@@ -24,6 +30,11 @@ public class AndGateAdapter implements ComponentAdapter<GUIAndGate>
 		ReadEnd[] ins = new ReadEnd[inputPins.size()];
 		for (int i = 0; i < inputPins.size(); i++)
 			ins[i] = logicWiresPerPin.get(inputPins.get(i)).createReadOnlyEnd();
-		return new AndGate(timeline, params.gateProcessTime, out, ins);
+		return constructor.newComponent(timeline, params.gateProcessTime, out, ins);
+	}
+
+	public static interface ComponentConstructor
+	{
+		public Component newComponent(Timeline timeline, int processTime, ReadWriteEnd out, ReadEnd[] ins);
 	}
 }
