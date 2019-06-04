4afb2d5617fbf679b464ff727741e599e751b5e7
Fabian Stemmler <stemmler@in.tum.de>
2019-06-04 22:21:01 +0200

    Added nand and nor gate due to popular demand

---
+++ net.mograsim.logic.core/src/net/mograsim/logic/core/components/gates/NandGate.java
@@ -0,0 +1,14 @@
+package net.mograsim.logic.core.components.gates;
+
+import net.mograsim.logic.core.timeline.Timeline;
+import net.mograsim.logic.core.types.BitVector.BitVectorMutator;
+import net.mograsim.logic.core.wires.Wire.ReadEnd;
+import net.mograsim.logic.core.wires.Wire.ReadWriteEnd;
+
+public class NandGate extends MultiInputGate
+{
+	public NandGate(Timeline timeline, int processTime, ReadWriteEnd out, ReadEnd... in)
+	{
+		super(timeline, processTime, BitVectorMutator::nand, out, in);
+	}
+}
