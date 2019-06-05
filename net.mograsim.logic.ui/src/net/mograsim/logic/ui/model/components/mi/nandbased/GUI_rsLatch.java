09197853bc44edb4fab75dccc8b870fd377e7df9
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-06-05 17:38:36 +0200

    Updated GUI_rsLatch to SimpleRectangularSubmodelComponent

--- net.mograsim.logic.ui/src/net/mograsim/logic/ui/model/components/mi/nandbased/GUI_rsLatch.java
+++ net.mograsim.logic.ui/src/net/mograsim/logic/ui/model/components/mi/nandbased/GUI_rsLatch.java
@@ -3,80 +3,48 @@ package net.mograsim.logic.ui.model.components.mi.nandbased;
 import net.haspamelodica.swt.helper.swtobjectwrappers.Point;
 import net.mograsim.logic.ui.model.ViewModelModifiable;
 import net.mograsim.logic.ui.model.components.GUINandGate;
-import net.mograsim.logic.ui.model.components.SubmodelComponent;
+import net.mograsim.logic.ui.model.components.SimpleRectangularSubmodelComponent;
 import net.mograsim.logic.ui.model.wires.GUIWire;
 import net.mograsim.logic.ui.model.wires.Pin;
 import net.mograsim.logic.ui.model.wires.WireCrossPoint;
 
-public class GUI_rsLatch extends SubmodelComponent
+public class GUI_rsLatch extends SimpleRectangularSubmodelComponent
 {
-	private final Pin pin_S;
-	private final Pin pin_R;
-	private final Pin pinQ;
-	private final Pin pin_Q;
-
 	public GUI_rsLatch(ViewModelModifiable model)
 	{
-		super(model, "_rsLatch");
-		setSize(35, 25);
-		setSubmodelScale(.2);
-
-		Pin _S = addSubmodelInterface(1, 0, 5);
-		Pin _R = addSubmodelInterface(1, 0, 20);
-		Pin Q = addSubmodelInterface(1, 35, 5);
-		Pin _Q = addSubmodelInterface(1, 35, 20);
-
-		this.pin_S = getSupermodelPin(_S);
-		this.pin_R = getSupermodelPin(_R);
-		this.pinQ = getSupermodelPin(Q);
-		this.pin_Q = getSupermodelPin(_Q);
-
-		initSubmodelComponents(_S, _R, Q, _Q);
+		super(model, 1, "_rsLatch");
+		setSubmodelScale(.4);
+		setInputCount(2);
+		setOutputCount(2);
+		initSubmodelComponents();
 	}
 
 	@SuppressWarnings("unused") // for GUIWires being created
-	private void initSubmodelComponents(Pin _S, Pin _R, Pin Q, Pin _Q)
+	private void initSubmodelComponents()
 	{
+		Pin _S = getInputSubmodelPins().get(0);
+		Pin _R = getInputSubmodelPins().get(1);
+		Pin Q = getOutputSubmodelPins().get(0);
+		Pin _Q = getOutputSubmodelPins().get(1);
+
 		GUINandGate nand1 = new GUINandGate(submodelModifiable, 1);
 		GUINandGate nand2 = new GUINandGate(submodelModifiable, 1);
 
 		WireCrossPoint cp1 = new WireCrossPoint(submodelModifiable, 1);
 		WireCrossPoint cp2 = new WireCrossPoint(submodelModifiable, 1);
 
-		nand1.moveTo(80, 20);
-		nand2.moveTo(80, 85);
-		cp1.moveTo(120, 30);
-		cp2.moveTo(120, 95);
+		nand1.moveTo(10, 7.5);
+		nand2.moveTo(40, 12.5);
+		cp1.moveTo(35, 17.5);
+		cp2.moveTo(65, 37.5);
 
-		new GUIWire(submodelModifiable, _S, nand1.getInputPins().get(0));
-		new GUIWire(submodelModifiable, _R, nand2.getInputPins().get(1));
-		new GUIWire(submodelModifiable, nand1.getOutputPin(), cp1);
-		new GUIWire(submodelModifiable, nand2.getOutputPin(), cp2);
-		new GUIWire(submodelModifiable, cp1, nand2.getInputPins().get(0), new Point(120, 50), new Point(60, 75),
-				new Point(60, 90));
-		new GUIWire(submodelModifiable, cp2, nand1.getInputPins().get(1), new Point(120, 75), new Point(60, 50),
-				new Point(60, 35));
-		new GUIWire(submodelModifiable, cp1, Q, new Point(150, 30), new Point(150, 25));
-		new GUIWire(submodelModifiable, cp2, _Q, new Point(150, 95), new Point(150, 100));
-	}
-
-	public Pin getPin_S()
-	{
-		return pin_S;
-	}
-
-	public Pin getPin_R()
-	{
-		return pin_R;
-	}
-
-	public Pin getPinQ()
-	{
-		return pinQ;
-	}
-
-	public Pin getPin_Q()
-	{
-		return pin_Q;
+		new GUIWire(submodelModifiable, _S, nand1.getInputPins().get(0), new Point[0]);
+		new GUIWire(submodelModifiable, _R, nand2.getInputPins().get(1), new Point(35, 37.5), new Point(35, 27.5));
+		new GUIWire(submodelModifiable, nand1.getOutputPin(), cp1, new Point[0]);
+		new GUIWire(submodelModifiable, nand2.getOutputPin(), cp2, new Point(65, 22.5));
+		new GUIWire(submodelModifiable, cp1, nand2.getInputPins().get(0), new Point[0]);
+		new GUIWire(submodelModifiable, cp2, nand1.getInputPins().get(1), new Point(65, 42.5), new Point(5, 42.5), new Point(5, 22.5));
+		new GUIWire(submodelModifiable, cp1, Q, new Point(35, 17.5), new Point(35, 7.5), new Point(65, 7.5), new Point(65, 12.5));
+		new GUIWire(submodelModifiable, cp2, _Q, new Point[0]);
 	}
 }
