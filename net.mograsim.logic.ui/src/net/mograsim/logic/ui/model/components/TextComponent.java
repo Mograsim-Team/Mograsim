838c017db855038424e4948e1d33898c8dda023f
Christian Femers <femers@in.tum.de>
2019-06-29 03:29:05 +0200

    Added getBounds() to TextComponent

--- net.mograsim.logic.ui/src/net/mograsim/logic/ui/model/components/TextComponent.java
+++ net.mograsim.logic.ui/src/net/mograsim/logic/ui/model/components/TextComponent.java
@@ -13,6 +13,7 @@ import net.mograsim.preferences.Preferences;
 public class TextComponent extends GUIComponent
 {
 	private final String text;
+	private Point textExtent;
 
 	public TextComponent(ViewModelModifiable model, String text)
 	{
@@ -23,11 +24,19 @@ public class TextComponent extends GUIComponent
 	@Override
 	public void render(GeneralGC gc, Rectangle visibleRegion)
 	{
-		Point textExtent = gc.textExtent(text);
+		textExtent = gc.textExtent(text);
 		Color textColor = Preferences.current().getColor("net.mograsim.logic.ui.color.text");
 		if (textColor != null)
 			gc.setForeground(textColor);
 		gc.drawText(text, getPosX() - textExtent.x / 2, getPosY() - textExtent.y / 2, true);
+	}
+
+	@Override
+	public Rectangle getBounds()
+	{
+		if (textExtent == null)
+			return super.getBounds();
+		return new Rectangle(getPosX() - textExtent.x / 2, getPosY() - textExtent.y / 2, textExtent.x, textExtent.y);
 	}
 
 	static
