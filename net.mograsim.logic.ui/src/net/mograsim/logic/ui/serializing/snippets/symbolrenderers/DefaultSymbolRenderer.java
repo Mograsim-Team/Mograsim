3ecb57b6d827c020dcb3d6cec087f812f73a9edc
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-07-04 16:12:43 +0200

    Made Renderers usable without JSON

---
+++ net.mograsim.logic.ui/src/net/mograsim/logic/ui/serializing/snippets/symbolrenderers/DefaultSymbolRenderer.java
@@ -0,0 +1,42 @@
+package net.mograsim.logic.ui.serializing.snippets.symbolrenderers;
+
+import net.haspamelodica.swt.helper.gcs.GeneralGC;
+import net.haspamelodica.swt.helper.swtobjectwrappers.Point;
+import net.haspamelodica.swt.helper.swtobjectwrappers.Rectangle;
+import net.mograsim.logic.ui.model.components.GUIComponent;
+import net.mograsim.logic.ui.model.components.submodels.SubmodelComponent;
+import net.mograsim.logic.ui.serializing.CodeSnippetSupplier;
+import net.mograsim.logic.ui.serializing.snippets.Renderer;
+import net.mograsim.logic.ui.serializing.snippets.SnippetSupplier;
+import net.mograsim.preferences.ColorDefinition;
+import net.mograsim.preferences.ColorManager;
+import net.mograsim.preferences.Preferences;
+
+public class DefaultSymbolRenderer implements Renderer
+{
+	private static final String id = "<Symbol\nunknown>";
+
+	private final GUIComponent component;
+
+	public DefaultSymbolRenderer(SubmodelComponent component, @SuppressWarnings("unused") Void params)
+	{
+		this.component = component;
+	}
+
+	@Override
+	public void render(GeneralGC gc, Rectangle visibleRegion)
+	{
+		ColorDefinition fg = Preferences.current().getColorDefinition("net.mograsim.logic.ui.color.text");
+		if (fg != null)
+			gc.setForeground(ColorManager.current().toColor(fg));
+		Point idSize = gc.textExtent(id);
+		Rectangle bounds = component.getBounds();
+		gc.drawText(id, bounds.x + (bounds.width - idSize.x) / 2, bounds.y + (bounds.height - idSize.y) / 2, true);
+	}
+
+	static
+	{
+		CodeSnippetSupplier.symbolRendererProviderSupplier.setSnippetProvider(DefaultSymbolRenderer.class.getCanonicalName(),
+				SnippetSupplier.create(Void.class, DefaultSymbolRenderer::new));
+	}
+}
