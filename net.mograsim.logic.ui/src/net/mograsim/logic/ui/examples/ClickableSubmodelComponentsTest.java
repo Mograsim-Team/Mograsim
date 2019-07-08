05f3deb7ee3a0b7162c75e1963be9a739618e3df
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-07-08 12:52:43 +0200

    Small fix in ClickableSubmodelComponentsTest

--- net.mograsim.logic.ui/src/net/mograsim/logic/ui/examples/ClickableSubmodelComponentsTest.java
+++ net.mograsim.logic.ui/src/net/mograsim/logic/ui/examples/ClickableSubmodelComponentsTest.java
@@ -5,6 +5,7 @@ import net.mograsim.logic.ui.model.ViewModelModifiable;
 import net.mograsim.logic.ui.model.components.atomic.GUIBitDisplay;
 import net.mograsim.logic.ui.model.components.atomic.GUIManualSwitch;
 import net.mograsim.logic.ui.model.components.submodels.SimpleRectangularSubmodelComponent;
+import net.mograsim.logic.ui.model.wires.GUIWire;
 
 public class ClickableSubmodelComponentsTest
 {
@@ -15,6 +16,7 @@ public class ClickableSubmodelComponentsTest
 
 	public static void createExample(ViewModelModifiable model)
 	{
+		@SuppressWarnings("unused") // GUIWire
 		SimpleRectangularSubmodelComponent comp = new SimpleRectangularSubmodelComponent(model, 1, "")
 		{
 			{
@@ -26,6 +28,8 @@ public class ClickableSubmodelComponentsTest
 
 				sw.moveTo(10, 5);
 				bd.moveTo(50, 5);
+
+				new GUIWire(submodelModifiable, sw.getOutputPin(), bd.getInputPin());
 
 			}
 		};
