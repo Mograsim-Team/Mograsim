e2d193c0ae869ab9d3d8d50c3d193c0c164ac846
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-08-10 15:10:11 +0200

    Implemented GUIor12

--- net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/GUIComponentTestbench.java
+++ net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/GUIComponentTestbench.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import net.mograsim.logic.model.SimpleLogicUIStandalone;
-import net.mograsim.logic.model.am2900.components.am2910.GUIAm2910RegCntr;
+import net.mograsim.logic.model.am2900.components.GUIor12;
 import net.mograsim.logic.model.model.ViewModelModifiable;
 import net.mograsim.logic.model.model.components.GUIComponent;
 import net.mograsim.logic.model.model.components.atomic.GUIBitDisplay;
@@ -26,7 +26,7 @@ public class GUIComponentTestbench
 	public static void createTestbench(ViewModelModifiable model)
 	{
 //		GUIComponent comp = IndirectGUIComponentCreator.createComponent(model, "GUIAm2901", "Am2901");
-		GUIComponent comp = new GUIAm2910RegCntr(model, "RegCntr");
+		GUIComponent comp = new GUIor12(model, "or12");
 
 		// guess which pins are outputs and which are inputs
 		// TODO this code exists four times... but it seems too "hacky" to put it in a helper class
