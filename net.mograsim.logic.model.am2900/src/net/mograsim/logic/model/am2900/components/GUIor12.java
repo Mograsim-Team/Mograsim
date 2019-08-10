48be96dd026d8fe63067088a8a546009109de6bb
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-08-10 15:10:11 +0200

    Implemented GUIor12

---
+++ net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/am2900/components/GUIor12.java
@@ -0,0 +1,52 @@
+package net.mograsim.logic.model.am2900.components;
+
+import static net.mograsim.logic.core.types.Bit.ONE;
+import static net.mograsim.logic.core.types.Bit.U;
+import static net.mograsim.logic.core.types.Bit.X;
+import static net.mograsim.logic.core.types.Bit.Z;
+import static net.mograsim.logic.core.types.Bit.ZERO;
+
+import java.util.Map;
+
+import net.mograsim.logic.core.types.Bit;
+import net.mograsim.logic.core.wires.Wire.ReadEnd;
+import net.mograsim.logic.core.wires.Wire.ReadWriteEnd;
+import net.mograsim.logic.model.model.ViewModelModifiable;
+import net.mograsim.logic.model.model.components.atomic.SimpleRectangularHardcodedGUIComponent;
+import net.mograsim.logic.model.model.wires.Pin;
+import net.mograsim.logic.model.snippets.symbolrenderers.PinNamesSymbolRenderer.PinNamesParams.Position;
+
+public class GUIor12 extends SimpleRectangularHardcodedGUIComponent
+{
+	public GUIor12(ViewModelModifiable model, String name)
+	{
+		super(model, name, ">0");
+		setSize(35, 20);
+		addPin(new Pin(this, "D", 12, 0, 10), Usage.INPUT, Position.RIGHT);
+		addPin(new Pin(this, "Y", 1, 30, 10), Usage.OUTPUT, Position.LEFT);
+	}
+
+	@Override
+	protected Object recalculate(Object lastState, Map<String, ReadEnd> readEnds, Map<String, ReadWriteEnd> readWriteEnds)
+	{
+		readWriteEnds.get("Y").feedSignals(getResult(readEnds.get("D").getValues().getBits()));
+		return null;
+	}
+
+	private static Bit getResult(Bit[] DValArr)
+	{
+		for (int i = 0; i < 12; i++)
+			if (DValArr[i] == X)
+				return X;
+		for (int i = 0; i < 12; i++)
+			if (DValArr[i] == U)
+				return U;
+		for (int i = 0; i < 12; i++)
+			if (DValArr[i] == ONE)
+				return ONE;
+		for (int i = 0; i < 12; i++)
+			if (DValArr[i] == Z)
+				return X;
+		return ZERO;
+	}
+}
