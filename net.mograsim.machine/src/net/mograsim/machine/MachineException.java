08bf9a8d3c15a622105d63bc0d9d0f59c0fae6fd
Fabian Stemmler <stemmler@in.tum.de>
2019-08-24 18:13:52 +0200

    Added MachineException and MemoryException

---
+++ net.mograsim.machine/src/net/mograsim/machine/MachineException.java
@@ -0,0 +1,25 @@
+package net.mograsim.machine;
+
+public class MachineException extends RuntimeException
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6224307211078376836L;
+
+	public MachineException()
+	{
+		super();
+	}
+	
+	public MachineException(String message)
+	{
+		super(message);
+	}
+	
+	public MachineException(Throwable cause)
+	{
+		super(cause);
+	}
+}
