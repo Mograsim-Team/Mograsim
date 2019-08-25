df3df1ed9dca75fdd6d3e2bf66c4e0e5113d99e7
Fabian Stemmler <stemmler@in.tum.de>
2019-08-25 17:21:55 +0200

    MainMemory now supports cells being accessed as BigIntegers

--- net.mograsim.machine/src/net/mograsim/machine/MainMemory.java
+++ net.mograsim.machine/src/net/mograsim/machine/MainMemory.java
@@ -1,10 +1,14 @@
 package net.mograsim.machine;
 
+import java.math.BigInteger;
+
 import net.mograsim.logic.core.types.BitVector;
 
 public interface MainMemory {
 	
 	public BitVector getCell(long address);
 	public void setCell(long address, BitVector word);
+	public BigInteger getCellAsBigInteger(long address);
+	public void setCellAsBigInteger(long address, BigInteger word);
 	public MainMemoryDefinition getDefinition();
 }
