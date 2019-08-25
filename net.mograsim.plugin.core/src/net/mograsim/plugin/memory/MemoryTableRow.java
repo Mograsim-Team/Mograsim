635127c46d0425d14c14921a6608bf9a75c18e95
Fabian Stemmler <stemmler@in.tum.de>
2019-08-25 17:30:07 +0200

    Added Memory View to the plugin

---
+++ net.mograsim.plugin.core/src/net/mograsim/plugin/memory/MemoryTableRow.java
@@ -0,0 +1,20 @@
+package net.mograsim.plugin.memory;
+
+import net.mograsim.machine.MainMemory;
+
+public class MemoryTableRow
+{
+	public final long address;
+	private final MainMemory memory;
+
+	public MemoryTableRow(long address, MainMemory memory)
+	{
+		this.address = address;
+		this.memory = memory;
+	}
+
+	public MainMemory getMemory()
+	{
+		return memory;
+	}
+}
---
+++ net.mograsim.plugin.core/src/net/mograsim/plugin/memory/NumberVerifyListener.java
@@ -0,0 +1,26 @@
+package net.mograsim.plugin.memory;
+
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.widgets.Text;
+
+import net.mograsim.plugin.asm.AsmNumberUtil;
+import net.mograsim.plugin.asm.AsmNumberUtil.NumberType;
+
+public class NumberVerifyListener implements VerifyListener
+{
+
+	@Override
+	public void verifyText(VerifyEvent e)
+	{
+		String text = computeModifiedText(e);
+		e.doit = !NumberType.NONE.equals(AsmNumberUtil.prefixOfType(text));
+	}
+
+	private static String computeModifiedText(VerifyEvent e)
+	{
+		String modifiedText = ((Text) e.getSource()).getText();
+		modifiedText = modifiedText.substring(0, e.start).concat(e.text).concat(modifiedText.substring(e.end));
+		return modifiedText;
+	}
+}
