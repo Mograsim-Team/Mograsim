7afd6ddbce7efbded4c37f8abd69e0fb7e5f2983
Fabian Stemmler <stemmler@in.tum.de>
2019-08-25 17:31:13 +0200

    Fixed WordAddressableMemoryTest
    Adjusted for new WordAddressableMemoryComponent constructor, which
    expects a MainMemoryDefinition

--- net.mograsim.machine/test/net/mograsim/machine/standard/memory/WordAddressableMemoryTest.java
+++ net.mograsim.machine/test/net/mograsim/machine/standard/memory/WordAddressableMemoryTest.java
@@ -12,6 +12,7 @@ import net.mograsim.logic.core.types.Bit;
 import net.mograsim.logic.core.types.BitVector;
 import net.mograsim.logic.core.wires.Wire;
 import net.mograsim.logic.core.wires.Wire.ReadWriteEnd;
+import net.mograsim.machine.DefaultMainMemoryDefinition;
 
 class WordAddressableMemoryTest {
 	
@@ -27,7 +28,7 @@ class WordAddressableMemoryTest {
 		ReadWriteEnd dataI = data.createReadWriteEnd();
 		ReadWriteEnd addressI = address.createReadWriteEnd();
 
-		WordAddressableMemoryComponent memory = new WordAddressableMemoryComponent(t, 4, 4096L, Long.MAX_VALUE, data.createReadWriteEnd(),
+		WordAddressableMemoryComponent memory = new WordAddressableMemoryComponent(t, 4, new DefaultMainMemoryDefinition(64, 16, 4096L, Long.MAX_VALUE), data.createReadWriteEnd(),
 				rW.createReadOnlyEnd(), address.createReadOnlyEnd());
 
 		Random r = new Random();
