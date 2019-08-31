be0ec56f1a71a1370ee0fd2f68f840995499b7ea
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-08-31 22:33:14 +0200

    GUIdff12 now has a HighLevelState

--- net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/am2900/components/GUIdff12.java
+++ net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/am2900/components/GUIdff12.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 import net.mograsim.logic.core.types.Bit;
+import net.mograsim.logic.core.types.BitVector;
 import net.mograsim.logic.core.wires.Wire.ReadEnd;
 import net.mograsim.logic.core.wires.Wire.ReadWriteEnd;
 import net.mograsim.logic.model.model.ViewModelModifiable;
@@ -47,6 +48,34 @@ public class GUIdff12 extends SimpleRectangularHardcodedGUIComponent
 		QC[12] = CVal;
 
 		return QC;
+	}
+
+	@Override
+	protected Object getHighLevelState(Object state, String stateID)
+	{
+		switch (stateID)
+		{
+		case "q":
+			return BitVector.of(Arrays.copyOfRange((Bit[]) state, 0, 12));
+		default:
+			return super.getHighLevelState(state, stateID);
+		}
+	}
+
+	@Override
+	protected Object setHighLevelState(Object lastState, String stateID, Object newHighLevelState)
+	{
+		switch (stateID)
+		{
+		case "q":
+			BitVector newHighLevelStateCasted = (BitVector) newHighLevelState;
+			if (newHighLevelStateCasted.length() != 12)
+				throw new IllegalArgumentException("Expected BitVector of length 12, not " + newHighLevelStateCasted.length());
+			System.arraycopy(newHighLevelStateCasted.getBits(), 0, lastState, 0, 12);
+			return lastState;
+		default:
+			return super.setHighLevelState(lastState, stateID, newHighLevelState);
+		}
 	}
 
 	static
