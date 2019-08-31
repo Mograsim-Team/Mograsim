9af4c63c9f80205cb016568cae4823e1d42b6b30
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-08-31 17:22:23 +0200

    Removed workaround for rendering bug because the bug is fixed

--- net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/FusionNotWorkingTest.java
+++ net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/FusionNotWorkingTest.java
@@ -1,13 +1,8 @@
 package net.mograsim.logic.model.examples;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import net.haspamelodica.swt.helper.gcs.GeneralGC;
 import net.haspamelodica.swt.helper.swtobjectwrappers.Rectangle;
-import net.mograsim.logic.core.timeline.TimelineEventHandler;
-import net.mograsim.logic.model.LogicUICanvas;
 import net.mograsim.logic.model.SimpleLogicUIStandalone;
-import net.mograsim.logic.model.SimpleLogicUIStandalone.VisualisationObjects;
 import net.mograsim.logic.model.model.ViewModelModifiable;
 import net.mograsim.logic.model.serializing.DeserializedSubmodelComponent;
 import net.mograsim.logic.model.serializing.IdentifierGetter;
@@ -18,7 +13,7 @@ public class FusionNotWorkingTest
 {
 	public static void main(String[] args)
 	{
-		SimpleLogicUIStandalone.executeVisualisation(FusionNotWorkingTest::create, FusionNotWorkingTest::beforeRun);
+		SimpleLogicUIStandalone.executeVisualisation(FusionNotWorkingTest::create);
 	}
 
 	public static void create(ViewModelModifiable model)
@@ -42,19 +37,5 @@ public class FusionNotWorkingTest
 				// do nothing
 			}
 		});
-	}
-
-	public static void beforeRun(VisualisationObjects vis)
-	{
-		// TODO workaround for rendering bug
-		AtomicReference<TimelineEventHandler> functionRef = new AtomicReference<>();
-		LogicUICanvas logicUICanvas = vis.gui.getLogicUICanvas();
-		TimelineEventHandler function = e ->
-		{
-			logicUICanvas.redrawThreadsafe();
-			vis.timeline.addEvent(functionRef.get(), 50);
-		};
-		functionRef.set(function);
-		vis.timeline.addEvent(function, 0);
 	}
 }
