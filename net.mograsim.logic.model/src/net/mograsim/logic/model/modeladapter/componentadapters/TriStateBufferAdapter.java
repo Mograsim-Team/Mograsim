7178a0e8a449ff5059e4af7b110f25ecc9d9a2e6
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-08-31 15:42:02 +0200

    Found a bug in logic.core

--- net.mograsim.logic.model/src/net/mograsim/logic/model/modeladapter/componentadapters/TriStateBufferAdapter.java
+++ net.mograsim.logic.model/src/net/mograsim/logic/model/modeladapter/componentadapters/TriStateBufferAdapter.java
@@ -2,6 +2,7 @@ package net.mograsim.logic.model.modeladapter.componentadapters;
 
 import java.util.Map;
 
+import net.mograsim.logic.core.LogicObserver;
 import net.mograsim.logic.core.components.TriStateBuffer;
 import net.mograsim.logic.core.timeline.Timeline;
 import net.mograsim.logic.core.wires.Wire;
@@ -13,7 +14,6 @@ import net.mograsim.logic.model.modeladapter.LogicModelParameters;
 
 public class TriStateBufferAdapter implements ComponentAdapter<GUITriStateBuffer>
 {
-
 	@Override
 	public Class<GUITriStateBuffer> getSupportedClass()
 	{
@@ -29,6 +29,17 @@ public class TriStateBufferAdapter implements ComponentAdapter<GUITriStateBuffer
 		ReadEnd enable = logicWiresPerPin.get(guiTsb.getPin("EN")).createReadOnlyEnd();
 		ReadWriteEnd out = logicWiresPerPin.get(guiTsb.getPin("OUT")).createReadWriteEnd();
 		new TriStateBuffer(timeline, params.gateProcessTime, in, out, enable);
+		// TODO debug code below - remove
+		String msgHeader = '<' + guiTsb.name + ">: ";
+		LogicObserver obs = c ->
+		{
+			System.out.println(msgHeader + "wire states:");
+			System.out.println(msgHeader + "  in    : " + in.getValues());
+			System.out.println(msgHeader + "  enable: " + enable.getValues());
+			System.out.println(msgHeader + "  out   : " + out.getValues());
+		};
+		in.registerObserver(obs);
+		enable.registerObserver(obs);
+		out.registerObserver(obs);
 	}
-
 }
