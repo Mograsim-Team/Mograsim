a712f854c6666e2b42a277485f1fd407cf3e72ef
Christian Femers <femers@in.tum.de>
2019-09-03 06:55:04 +0200

    Make plugin work (in an inelegant way, need to figure out extensions

--- net.mograsim.plugin.core/src/net/mograsim/plugin/views/LogicUIPart.java
+++ net.mograsim.plugin.core/src/net/mograsim/plugin/views/LogicUIPart.java
@@ -1,8 +1,6 @@
 package net.mograsim.plugin.views;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -13,19 +11,15 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import net.haspamelodica.swt.helper.zoomablecanvas.helper.ZoomableCanvasUserInput;
-import net.mograsim.logic.core.timeline.Timeline;
 import net.mograsim.logic.model.LogicExecuter;
 import net.mograsim.logic.model.LogicUICanvas;
+import net.mograsim.logic.model.am2900.Am2900Loader;
 import net.mograsim.logic.model.model.ViewModelModifiable;
-import net.mograsim.logic.model.model.components.GUIComponent;
-import net.mograsim.logic.model.model.components.atomic.GUIBitDisplay;
-import net.mograsim.logic.model.model.components.atomic.GUIManualSwitch;
-import net.mograsim.logic.model.model.wires.GUIWire;
-import net.mograsim.logic.model.model.wires.Pin;
-import net.mograsim.logic.model.model.wires.PinUsage;
 import net.mograsim.logic.model.modeladapter.LogicModelParameters;
 import net.mograsim.logic.model.modeladapter.ViewLogicModelAdapter;
 import net.mograsim.logic.model.serializing.IndirectGUIComponentCreator;
+import net.mograsim.machine.MachineDefinition;
+import net.mograsim.machine.MachineRegistry;
 import net.mograsim.plugin.ThemePreferences;
 import net.mograsim.preferences.Preferences;
 
@@ -50,18 +44,18 @@ public class LogicUIPart extends ViewPart
 		// set preferences
 		Preferences.setPreferences(new ThemePreferences(PlatformUI.getWorkbench().getThemeManager().getCurrentTheme()));
 
-		// setup view model
-		ViewModelModifiable viewModel = new ViewModelModifiable();
-		createTestbench(viewModel);
+		Optional<MachineDefinition> mdo = MachineRegistry.getinstalledMachines().stream().findFirst();
 
-		// convert to logic model
+		Am2900Loader.setup();
+		ViewModelModifiable viewModelModifiable = new ViewModelModifiable();
+		IndirectGUIComponentCreator.createComponent(viewModelModifiable, "resource:Am2900Loader:/components/GUIAm2900.json");
 		LogicModelParameters params = new LogicModelParameters();
 		params.gateProcessTime = 50;
 		params.wireTravelTime = 10;
-		Timeline timeline = ViewLogicModelAdapter.convert(viewModel, params);
+//		timeline = ;
 
 		// initialize UI
-		ui = new LogicUICanvas(parent, SWT.NONE, viewModel);
+		ui = new LogicUICanvas(parent, SWT.NONE, viewModelModifiable);
 		ui.addTransformListener((x, y, z) -> part.setDirty(z < 1));
 		ZoomableCanvasUserInput userInput = new ZoomableCanvasUserInput(ui);
 		userInput.buttonDrag = 3;
@@ -69,7 +63,7 @@ public class LogicUIPart extends ViewPart
 		userInput.enableUserInput();
 
 		// initialize executer
-		exec = new LogicExecuter(timeline);
+		exec = new LogicExecuter(ViewLogicModelAdapter.convert(viewModelModifiable, params));
 
 		// run it
 		exec.startLiveExecution();
@@ -79,36 +73,5 @@ public class LogicUIPart extends ViewPart
 	public void setFocus()
 	{
 		ui.setFocus();
-	}
-
-	@SuppressWarnings("unused") // for GUIWires being created
-	public static void createTestbench(ViewModelModifiable model)
-	{
-		GUIComponent comp = IndirectGUIComponentCreator.createComponent(model, "GUIAm2901");
-
-		List<String> inputPinNames = new ArrayList<>();
-		List<String> outputPinNames = new ArrayList<>();
-		for (Pin p : comp.getPins().values())
-			if (p.usage == PinUsage.INPUT)
-				inputPinNames.add(p.name);
-			else
-				outputPinNames.add(p.name);
-
-		inputPinNames.sort(Comparator.comparing(comp::getPin, Comparator.comparing(Pin::getRelY)));
-		outputPinNames.sort(Comparator.comparing(comp::getPin, Comparator.comparing(Pin::getRelY)));
-
-		comp.moveTo(100, 0);
-		for (int i = 0; i < inputPinNames.size(); i++)
-		{
-			GUIManualSwitch sw = new GUIManualSwitch(model, 1);
-			sw.moveTo(0, 20 * i);
-			new GUIWire(model, comp.getPin(inputPinNames.get(i)), sw.getOutputPin());
-		}
-		for (int i = 0; i < outputPinNames.size(); i++)
-		{
-			GUIBitDisplay bd = new GUIBitDisplay(model, 1);
-			bd.moveTo(200, 20 * i);
-			new GUIWire(model, comp.getPin(outputPinNames.get(i)), bd.getInputPin());
-		}
 	}
 }
