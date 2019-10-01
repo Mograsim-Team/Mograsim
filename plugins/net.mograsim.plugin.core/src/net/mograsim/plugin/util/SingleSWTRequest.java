ff24f544796d5cc8898c6353d479b7eb3662e86f
Fabian Stemmler <stemmler@in.tum.de>
2019-10-01 02:42:21 +0200

    SingleSWTRequest utility class now actually does what it is supposed to

--- plugins/net.mograsim.plugin.core/src/net/mograsim/plugin/util/SingleSWTRequest.java
+++ plugins/net.mograsim.plugin.core/src/net/mograsim/plugin/util/SingleSWTRequest.java
@@ -4,14 +4,20 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.swt.widgets.Display;
 
+/**
+ * A utility class that requests the asynchronous execution of a Runnable in the SWT Thread. Making a new request before the old one is
+ * processed will override the old request.
+ */
 public class SingleSWTRequest
 {
 	private AtomicBoolean waiting = new AtomicBoolean();
+	private Runnable request;
 
 	public void request(Runnable request)
 	{
 		synchronized (waiting)
 		{
+			this.request = request;
 			if (!waiting.get())
 			{
 				waiting.set(true);
@@ -20,7 +26,7 @@ public class SingleSWTRequest
 					synchronized (waiting)
 					{
 						waiting.set(false);
-						request.run();
+						this.request.run();
 					}
 				});
 			}
