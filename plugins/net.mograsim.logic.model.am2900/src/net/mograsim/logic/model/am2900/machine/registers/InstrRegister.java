112d36c78a7f1b7c2ad4932d819c7436453ab820
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-10-07 02:25:43 +0200

    Added register for IR

---
+++ plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/am2900/machine/registers/InstrRegister.java
@@ -0,0 +1,14 @@
+package net.mograsim.logic.model.am2900.machine.registers;
+
+import net.mograsim.logic.model.am2900.machine.registers.Am2900Register;
+import net.mograsim.machine.registers.HighLevelStateBasedRegister;
+
+public class InstrRegister extends HighLevelStateBasedRegister implements Am2900Register
+{
+	public static final InstrRegister instance = new InstrRegister();
+
+	private InstrRegister()
+	{
+		super("IR", "ir.q", 16);
+	}
+}
