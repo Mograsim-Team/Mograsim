f98aba44258dd800875cf954f949221ab42c6728
Daniel Kirschten <daniel.kirschten@gmx.de>
2019-10-07 02:24:16 +0200

    Added register for muIR

---
+++ plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/am2900/machine/registers/muInstrRegister.java
@@ -0,0 +1,14 @@
+package net.mograsim.logic.model.am2900.machine.registers;
+
+import net.mograsim.logic.model.am2900.machine.registers.Am2900Register;
+import net.mograsim.machine.registers.HighLevelStateBasedRegister;
+
+public class muInstrRegister extends HighLevelStateBasedRegister implements Am2900Register
+{
+	public static final muInstrRegister instance = new muInstrRegister();
+
+	private muInstrRegister()
+	{
+		super("\u00b5IR", "muir_2.q", 80);// 00b5 = micro symbol
+	}
+}
