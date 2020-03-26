267267d2560ecba360e040ddd11135a1a2e01574
Daniel Kirschten <daniel.kirschten@gmx.de>
2020-03-26 01:28:17 +0100

    Implemented a part of the Am2910InstrPLA on gate level

--- plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
+++ plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
@@ -27,7 +27,7 @@ public class ModelComponentTestbench
 	{
 		Am2900Loader.setup();
 //		ModelComponent comp = new StrictAm2900MachineDefinition().createNew(model).getAm2900();
-		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "xnor");
+		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "Am2910InstrPLA_");
 
 		List<String> inputPinNames = new ArrayList<>();
 		List<String> outputPinNames = new ArrayList<>();
