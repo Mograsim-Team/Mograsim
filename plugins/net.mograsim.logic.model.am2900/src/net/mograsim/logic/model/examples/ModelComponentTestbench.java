8770c9fadaa74f095f9742ec1883a2a01a6472f0
Daniel Kirschten <daniel.kirschten@gmx.de>
2020-04-11 15:21:41 +0200

    Implemented sel4_12 gate-based

--- plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
+++ plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
@@ -27,7 +27,7 @@ public class ModelComponentTestbench
 	{
 		Am2900Loader.setup();
 //		ModelComponent comp = new StrictAm2900MachineDefinition().createNew(model).getAm2900();
-		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "sel4_4");
+		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "sel4_12");
 
 		List<String> inputPinNames = new ArrayList<>();
 		List<String> outputPinNames = new ArrayList<>();
