b2da7f73b1d13272ca1571513644ca0838af7a38
Daniel Kirschten <daniel.kirschten@gmx.de>
2020-04-11 14:25:26 +0200

    Implemented sel4

--- plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
+++ plugins/net.mograsim.logic.model.am2900/src/net/mograsim/logic/model/examples/ModelComponentTestbench.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import net.mograsim.logic.model.SimpleLogicUIStandalone;
 import net.mograsim.logic.model.am2900.Am2900Loader;
-import net.mograsim.logic.model.am2900.machine.StrictAm2900MachineDefinition;
 import net.mograsim.logic.model.model.LogicModelModifiable;
 import net.mograsim.logic.model.model.components.ModelComponent;
 import net.mograsim.logic.model.model.components.atomic.ModelBitDisplay;
@@ -14,6 +13,7 @@ import net.mograsim.logic.model.model.components.atomic.ModelManualSwitch;
 import net.mograsim.logic.model.model.wires.ModelWire;
 import net.mograsim.logic.model.model.wires.Pin;
 import net.mograsim.logic.model.model.wires.PinUsage;
+import net.mograsim.logic.model.serializing.IndirectModelComponentCreator;
 
 public class ModelComponentTestbench
 {
@@ -26,8 +26,8 @@ public class ModelComponentTestbench
 	public static void createTestbench(LogicModelModifiable model)
 	{
 		Am2900Loader.setup();
-		ModelComponent comp = new StrictAm2900MachineDefinition().createNew(model).getAm2900();
-//		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "inc12");
+//		ModelComponent comp = new StrictAm2900MachineDefinition().createNew(model).getAm2900();
+		ModelComponent comp = IndirectModelComponentCreator.createComponent(model, "sel4");
 
 		List<String> inputPinNames = new ArrayList<>();
 		List<String> outputPinNames = new ArrayList<>();
